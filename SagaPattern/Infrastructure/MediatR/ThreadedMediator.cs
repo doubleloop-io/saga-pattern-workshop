1a48eed148c6094a82211a9ac2da44f312c0b84d
Andrea Vallotti <andrea.vallotti@gmail.com>
2019-05-14 14:58:10 +0200

    Log error in handler execution

--- SagaPattern/Infrastructure/MediatR/ThreadedMediator.cs
+++ SagaPattern/Infrastructure/MediatR/ThreadedMediator.cs
@@ -3,19 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
+using NLog;
 
 namespace SagaPattern.Infrastructure.MediatR
 {
     internal class ThreadedMediator : Mediator
     {
+        static readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
         public ThreadedMediator(ServiceFactory serviceFactory) : base(serviceFactory)
         {
         }
 
         protected override Task PublishCore(IEnumerable<Func<Task>> allHandlers)
         {
-            allHandlers.AsParallel().Select(handler => handler()).ToList();
+            allHandlers.AsParallel().Select(TryHandle).ToList();
             return Task.CompletedTask;
+        }
+
+        private async Task TryHandle(Func<Task> handler)
+        {
+            try
+            {
+                await handler();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+            }
         }
     }
 }
